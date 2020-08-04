f12ea9f5251958a93a90482450a21442d8a96e98
[디지캡]명성 <smyeong@digicaps.com>
2020-08-04 17:37:25 +0900

    git ignore backend env 제거 -> 추후 정리되면 추가예정

--- back/routes/api/hangangController.js
+++ back/routes/api/hangangController.js
@@ -5,11 +5,14 @@ var axios = require('axios');
 router.get('/hangang_data', async function (req, res, next) {
   try {
     const { data } = await axios.get(
-      `http://openapi.seoul.go.kr:8088/${process.env.API_KEY}/json/WPOSInformationTime/1/5/`
+      `${process.env.SEOUL_OPENAPI_URL}` +
+      `/${process.env.API_KEY}` +
+      '/json/WPOSInformationTime/1/5/'
     );
-    res.send(data.WPOSInformationTime.row);
+    return res.send(data.WPOSInformationTime.row);
   } catch (error) {
     console.error(error);
+    return res.json('error');
   }
 });
 
