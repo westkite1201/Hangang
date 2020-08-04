5df30fab0da0094247f0733d9e56c9f29c774709
[디지캡]명성 <smyeong@digicaps.com>
2020-08-04 16:54:36 +0900

    불필요 파일 삭제 및 코드삭제

--- back/config/serverConfig.js
+++
@@ -1,5 +0,0 @@
-export const openApiUrl = 'http://openapi.seoul.go.kr:8088'; // Local PC 개발 시 사용 설정, 단말로 테스트 시에 Local PC의 IP로 설정
-export const apiKey = '7a66754e717478743335686b515547';
-export const typeOfData = {
-    'json': 'json'
-};
