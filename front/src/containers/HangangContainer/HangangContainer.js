c9f06bf90d84b3bfeea3798c9db139d4d470bc63
seoyeon kim <westkitedev@gmail.com>
2020-08-04 17:05:46 +0900

    feat : cors 모듈 추가

--- front/src/containers/HangangContainer/HangangContainer.js
+++ front/src/containers/HangangContainer/HangangContainer.js
@@ -1,24 +1,22 @@
 import React, { Fragment } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
-//import {} from '../modules/hangang/reducer';
+import { GET_HANGANG_TEMP_REQUEST } from '../../modules/hangang/reducer';
 
 function HangangContainer() {
-  // const { todosArr, todoModalView } = useSelector((state) => state.todos);
-  // const dispatch = useDispatch();
+  //  const { todosArr, todoModalView } = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
 
-  // const handleClose = () => {
-  //   dispatch({
-  //     type: TODO_MODAL_CLOSE,
-  //     payload: {
-  //       todoItemIndex: 0,
-  //       isEdit: false
-  //     }
-  //   });
-  // };
+  const getTemp = () => {
+    dispatch({
+      type: GET_HANGANG_TEMP_REQUEST,
+      payload: {}
+    });
+  };
 
   return (
     <Fragment>
+      <button onClick={getTemp}>테스트 </button>
       <Div>HELLO 한강</Div>
     </Fragment>
   );
