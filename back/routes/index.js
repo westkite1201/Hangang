2fb333655ceb5b5746b8f750fe727341d327f6f0
[디지캡]명성 <smyeong@digicaps.com>
2020-08-04 16:07:33 +0900

    add to five river data get express

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+/* use modules */
+var axios = require('axios');
+var axiosExtension = require('axios-extensions');
+
+/* Create Cache instance with axios extension */
+var instance = axios.create({
+  baseURL: '/',
+  Accept: 'application/json',
+  headers: { 'Cache-Control': 'no-cache' }, //커스텅 캐싱을 원할 경우 no-cache
+  adapter: axiosExtension.cacheAdapterEnhancer(
+    axios.defaults.adapter,
+    { enabledByDefault: false }
+    //enabledByDefault false <=  모든 네트워크 요청에 캐싱된 데이터 사용 x
+  )
+});
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* GET Five Hangang data */
+router.get('/api/hangang_data', async function (req, res, next) {
+  try {
+    const { data } = await axios.get('http://openapi.seoul.go.kr:8088/7a66754e717478743335686b515547/json/WPOSInformationTime/1/5/');
+    res.send(data.WPOSInformationTime.row);
+
+  } catch (error) {
+    console.error(error);
+  }
+
+})
 
 module.exports = router;
