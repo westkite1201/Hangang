cac1057cd555f11100a400cb7095cd6d0ad8fa3c
seoyeon kim <westkitedev@gmail.com>
2020-08-04 17:21:36 +0900

    fix : cors 및 axios 호출 해결

--- back/app.js
+++ back/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+let hangangRouter = require('./routes/api/hangangController');
 var cors = require('cors')();
 var app = express();
 require('dotenv').config();
@@ -19,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-
+app.use('/api/hangang', hangangRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
--- front/src/lib/api/hangang.js
+++ front/src/lib/api/hangang.js
@@ -1,9 +1,9 @@
-import axios from './apiClient';
-
+//import axios from './apiClient';
+import axios from 'axios';
 export async function getHangangTemp() {
   //   const data = {
   //     todoNum: todoNum
   //   };
-  const response = await axios.get(`http://localhost:3030/api/getHangangTemp`);
+  const response = await axios.get(`http://localhost:3030/api/hangang/hangang_data`);
   return response.data; // 데이터 값을 바로 반환하도록 처리합니다.
 }
