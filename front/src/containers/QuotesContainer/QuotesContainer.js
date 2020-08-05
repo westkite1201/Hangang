185810e1a79a2ec7cfddde3422829b5f1b964bd4
seoyeon kim <westkitedev@gmail.com>
2020-08-05 11:33:04 +0900

    feat : react-slick 활용 명언추가 연동

---
+++ front/src/containers/QuotesContainer/QuotesContainer.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import Slider from 'react-slick';
+import { useDispatch, useSelector } from 'react-redux';
+import QuotesItem from '../../component/QuotesItem';
+import { GET_QUOTES_REQUEST } from '../../modules/hangang/reducer';
+
+const QuotesContainer = () => {
+  const { quotesData } = useSelector((state) => state.hangang);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch({
+      type: GET_QUOTES_REQUEST,
+      payload: {}
+    });
+  }, []);
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    vertical: true,
+    verticalSwiping: true,
+    swipeToSlide: true,
+    beforeChange: function (currentSlide, nextSlide) {
+      console.log('before change', currentSlide, nextSlide);
+    },
+    afterChange: function (currentSlide) {
+      console.log('after change', currentSlide);
+    }
+  };
+  const { data } = quotesData;
+  //console.log(data);
+  return (
+    <Slider {...settings}>
+      {data &&
+        data.map((quotes, index) => {
+          return <QuotesItem key={index} quotes={quotes} />;
+        })}
+    </Slider>
+  );
+};
+export default QuotesContainer;
--- front/src/modules/hangang/reducer.js
+++ front/src/modules/hangang/reducer.js
@@ -1,12 +1,17 @@
 import produce from 'immer';
 
 export const initialState = {
-  riverTempData: []
+  riverTempData: [],
+  quotesData: []
 };
 
 export const GET_HANGANG_TEMP_REQUEST = 'todos/GET_HANGANG_TEMP_REQUEST';
 export const GET_HANGANG_TEMP_SUCCESS = 'todos/GET_HANGANG_TEMP_SUCCESS';
 export const GET_HANGANG_TEMP_FAILURE = 'todos/GET_HANGANG_TEMP_FAILURE';
+
+export const GET_QUOTES_REQUEST = 'todos/GET_QUOTES_REQUEST';
+export const GET_QUOTES_SUCCESS = 'todos/GET_QUOTES_SUCCESS';
+export const GET_QUOTES_FAILURE = 'todos/GET_QUOTES_FAILURE';
 
 export default (state = initialState, action) => {
   return produce(state, (draft) => {
@@ -16,6 +21,14 @@ export default (state = initialState, action) => {
         break;
       }
       case GET_HANGANG_TEMP_FAILURE: {
+        break;
+      }
+
+      case GET_QUOTES_SUCCESS: {
+        draft.quotesData = action.payload;
+        break;
+      }
+      case GET_QUOTES_FAILURE: {
         break;
       }
 
