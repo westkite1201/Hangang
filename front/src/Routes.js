113e8d48333b47901a06d47e1ad43d60984cbf9c
seoyeon kim <westkitedev@gmail.com>
2020-08-05 15:55:28 +0900

    feat : react-spring animation 추가

--- front/src/Routes.js
+++ front/src/Routes.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
 import { Switch } from 'react-router-dom';
 import SubRoutes from './SubRoutes';
-
+import TopBar from './component/common/TopBar';
 const Routes = ({ history }) => {
   return (
     <div className="contbody_scroll">
+      <TopBar />
       <Switch>
         <SubRoutes history={history} />
       </Switch>
