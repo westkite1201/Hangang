9b5bd359dc84ef539f78662d06641478b587ccc6
seoyeon kim <westkitedev@gmail.com>
2020-08-06 15:59:15 +0900

    fix :env 용어 수정

--- front/src/configuration/clientConfig.js
+++ front/src/configuration/clientConfig.js
@@ -5,11 +5,11 @@
 module.exports = {
   endpoint: {
     web:
-      process.env.REACT_APP_MODE === 'develop'
+      process.env.REACT_APP_API_HOST === 'develop'
         ? 'http://127.0.0.1:3031'
         : 'https://www.hangang.site',
     api:
-      process.env.REACT_APP_MODE === 'develop'
+      process.env.REACT_APP_API_HOST === 'develop'
         ? 'http://127.0.0.1:3031/api'
         : 'https://www.hangang.site/api'
   }
