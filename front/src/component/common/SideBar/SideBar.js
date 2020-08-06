6eaa9fac4c573f8347e67b50295db5c65bc3d529
MasonMS <brightcastle91@gmail.com>
2020-08-06 16:25:45 +0900

    명언 GET 수정 1. 방식 변경 GET -> POST 2. 파라미터 추가 (accepted)

--- front/src/component/common/SideBar/SideBar.js
+++ front/src/component/common/SideBar/SideBar.js
@@ -21,14 +21,25 @@ const SideBar = () => {
         <a id="home" className="menu-item" href="/">
           Home
         </a>
+        <br/>
         <a id="about" className="menu-item" href="/about">
           About
         </a>
+        <br/>
         <a id="contact" className="menu-item" href="/contact">
           Contact
         </a>
+        <br/>
         <a onClick={showSettings} className="menu-item--small" href="">
           Settings
+        </a>
+        <br/>
+        <a className="menu-item--small" href="/admin">
+          Admin
+        </a>
+        <br/>
+        <a className="menu-item--small" href="/submit-qoutes">
+          Submit
         </a>
       </main>
     </Menu>
