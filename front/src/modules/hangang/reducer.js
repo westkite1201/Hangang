ae9c92a65f25a3bd1235a0df61b16d3d639c2aa7
MasonMS <brightcastle91@gmail.com>
2020-08-06 18:15:11 +0900

    admin 및 submit 페이지 일부 수정진행중

--- front/src/modules/hangang/reducer.js
+++ front/src/modules/hangang/reducer.js
@@ -12,6 +12,7 @@ export const GET_HANGANG_TEMP_FAILURE = 'todos/GET_HANGANG_TEMP_FAILURE';
 export const GET_QUOTES_REQUEST = 'todos/GET_QUOTES_REQUEST';
 export const GET_QUOTES_SUCCESS = 'todos/GET_QUOTES_SUCCESS';
 export const GET_QUOTES_FAILURE = 'todos/GET_QUOTES_FAILURE';
+export const GET_QUOTES_SUBMIT = 'todos/GET_QUOTES_SUBMIT';
 
 export default (state = initialState, action) => {
   return produce(state, (draft) => {
@@ -32,6 +33,10 @@ export default (state = initialState, action) => {
         break;
       }
 
+      case GET_QUOTES_SUBMIT: {
+        draft.quotesData = action.payload;
+      }
+      
       default: {
         break;
       }
