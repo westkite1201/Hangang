afad03758fd4e6d67bcd9db23bc578b0ab507dc8
seoyeon kim <westkitedev@gmail.com>
2020-08-06 10:02:22 +0900

    포트 변경

--- front/src/component/common/SideBar/SideBar.js
+++ front/src/component/common/SideBar/SideBar.js
@@ -11,19 +11,26 @@ const SideBar = () => {
   }
   // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
   return (
-    <Menu isOpen={isOpen} pushRotate>
-      <a id="home" className="menu-item" href="/">
-        Home
-      </a>
-      <a id="about" className="menu-item" href="/about">
-        About
-      </a>
-      <a id="contact" className="menu-item" href="/contact">
-        Contact
-      </a>
-      <a onClick={showSettings} className="menu-item--small" href="">
-        Settings
-      </a>
+    <Menu
+      pageWrapId={'page-wrap'}
+      outerContainerId={'outer-container'}
+      isOpen={isOpen}
+      pushRotate
+    >
+      <main id="page-wrap">
+        <a id="home" className="menu-item" href="/">
+          Home
+        </a>
+        <a id="about" className="menu-item" href="/about">
+          About
+        </a>
+        <a id="contact" className="menu-item" href="/contact">
+          Contact
+        </a>
+        <a onClick={showSettings} className="menu-item--small" href="">
+          Settings
+        </a>
+      </main>
     </Menu>
   );
 };
