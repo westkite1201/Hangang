4182e244ff1830dd4462981c6456103e12df9693
seoyeon kim <westkitedev@gmail.com>
2020-08-07 18:33:34 +0900

    feat : quotesGridview 추가

---
+++ front/src/containers/QuotesGridViewComponent/QuotesGridViewComponent.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+import { Grid } from '@material-ui/core';
+import { useDispatch, useSelector } from 'react-redux';
+import { GET_QUOTES_REQUEST } from '../../modules/hangang/reducer';
+import QuotesCard from '../../component/QuotesCard';
+
+const QuotesCardList = ({ quotesList }) => {
+  return quotesList.map((quotes, key) => {
+    return (
+      <Grid item xs={12} md={6} lg={4} key={key}>
+        <QuotesCard quotes={quotes} />;
+      </Grid>
+    );
+  });
+};
+
+/* 명언 그리드  */
+const QuotesGridViewComponent = () => {
+  const { quotesData } = useSelector((state) => state.hangang);
+  const { data: quotesList } = quotesData;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch({
+      type: GET_QUOTES_REQUEST,
+      payload: { accepted: '0' }
+    });
+  }, [dispatch]);
+
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={1} md={1} lg={1}></Grid>
+      <Grid item xs={10} md={10} lg={10}>
+        <Grid container spacing={3}>
+          {quotesList && <QuotesCardList quotesList={quotesList} />}
+        </Grid>
+      </Grid>
+      <Grid item xs={1} md={1} lg={1}></Grid>
+    </Grid>
+  );
+};
+export default QuotesGridViewComponent;
