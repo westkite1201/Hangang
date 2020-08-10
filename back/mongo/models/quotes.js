1e7104ebe975049f673f74f777a5575cd3e825c4
MasonMS <brightcastle91@gmail.com>
2020-08-10 11:46:52 +0900

    명언 컨트롤러, Model 수정

--- back/mongo/models/quotes.js
+++ back/mongo/models/quotes.js
@@ -8,32 +8,13 @@ const quotesSchema = new Schema({
     name: { type: String, required: true },
     word: { type: String, required: true },
     image: { type: String, required: false, default: '' },
-    accepted: { type: String, required: true, default: '1' },   // 0: 승인, 1: 대기, 2: 거절
-    insertTime: { type: Date, default: moment().tz('Asia/Seoul').format('YYYY-MM-DD HH:mm:ss') },
-    updateTime: { type: Date, default: moment().tz('Asia/Seoul').format('YYYY-MM-DD HH:mm:ss') }
+    accepted: { type: String, required: false, default: '1' },   // 0: 승인, 1: 대기, 2: 거절
+    insertTime: { type: String, default: moment().tz('Asia/Seoul').format('YYYY-MM-DD HH:mm:ss') },
+    updateTime: { type: String, default: moment().tz('Asia/Seoul').format('YYYY-MM-DD HH:mm:ss') },
+    status: { type: String, default: '0'}   // 소재상태 '0' 사용중, '1' 삭제됨
 }, {
     collection: 'QUOTES'
 });
 
-// Insert Quotes
-quotesSchema.statics.insertQuotes = function (payload) {
-    // this ==> model
-    const quote = new this(payload);
-    // return Promise
-    return quote.save();
-}
-
-// Find All Allow Quotes
-quotesSchema.statics.findAllQuotes = function (payload) {
-    return this.find({accepted: payload.accepted});
-}
-
-// Delete Quotes using _id
-quotesSchema.statics.deleteQoutes = function (payload) {
-    const id = payload.id;
-    return this.deleteOne({
-        _id: id
-    });
-}
 const model = mongoose.model;
 module.exports = model('Quotes', quotesSchema);
