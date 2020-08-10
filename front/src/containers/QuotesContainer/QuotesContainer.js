8958ac5123a2a96012110cab43ace650265fae42
seoyeon kim <westkitedev@gmail.com>
2020-08-10 18:47:07 +0900

    feat : slider button 왼,오른쪽 button 추가

--- front/src/containers/QuotesContainer/QuotesContainer.js
+++ front/src/containers/QuotesContainer/QuotesContainer.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Slider from 'react-slick';
 import { useDispatch, useSelector } from 'react-redux';
 import QuotesItem from '../../component/QuotesItem';
+import styled from 'styled-components';
 // import { GET_QUOTES_REQUEST } from '../../modules/hangang/reducer';
-
+//swiper
 const QuotesContainer = (props) => {
+  const slider1 = useRef();
   const { quotesData } = useSelector((state) => state.hangang);
   const dispatch = useDispatch();
   useEffect(() => {
@@ -33,13 +35,58 @@ const QuotesContainer = (props) => {
   };
   const { data } = quotesData;
   //console.log(data);
+
+  const next = () => {
+    if (slider1.current) {
+      slider1.current.slickNext();
+    }
+  };
+  const previous = () => {
+    if (slider1.current) {
+      slider1.current.slickPrev();
+    }
+  };
+
   return (
-    <Slider {...settings}>
-      {data &&
-        data.map((quotes, index) => {
-          return <QuotesItem key={index} quotes={quotes} />;
-        })}
-    </Slider>
+    <div>
+      <Slider {...settings} ref={(slider) => (slider1.current = slider)}>
+        {data &&
+          data.map((quotes, index) => {
+            return <QuotesItem key={index} quotes={quotes} />;
+          })}
+      </Slider>
+      <QuotesSliderButton>
+        <span onClick={previous}>
+          <svg width="48" height="48" fill="none" viewBox="0 0 48 48">
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M15.5 5.5l-6 6.5 6 6.5L14 20l-7.5-8L14 4l1.5 1.5z"
+              fill="#1b1c1d"
+            ></path>
+          </svg>
+        </span>
+        <span onClick={next}>
+          <svg width="48" height="48" fill="none" viewBox="0 0 48 48">
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M8.5 18.5l6-6.5-6-6.5L10 4l7.5 8-7.5 8-1.5-1.5z"
+              fill="#1b1c1d"
+            ></path>
+          </svg>
+        </span>
+      </QuotesSliderButton>
+    </div>
   );
 };
+const QuotesSliderButton = styled.div`
+  padding-left: 25px;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  span {
+    cursor: pointer;
+  }
+`;
 export default QuotesContainer;
