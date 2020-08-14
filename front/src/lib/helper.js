2ff5e1a4697b915a4b0b04d7a6713a79b96ea771
seoyeon kim <westkitedev@gmail.com>
2020-08-14 18:37:18 +0900

    Bannermaker proto 완성

--- front/src/lib/helper.js
+++ front/src/lib/helper.js
@@ -82,3 +82,15 @@ export function getContentCardType(pst_exps_typ_cd) {
       return 'default';
   }
 }
+
+export const getContrastYIQ = (color) => {
+  const r = parseInt(color.substr(0, 2), 16);
+  const g = parseInt(color.substr(2, 2), 16);
+  const b = parseInt(color.substr(4, 2), 16);
+  const yiq = (r * 299 + g * 587 + b * 114) / 1000;
+  return yiq >= 220 ? '#000000' : '#ffffff';
+};
+
+// eslint-disable-next-line
+export const getRandomHexColor = () =>
+  `#${`0${(~~(Math.random() * 16777215)).toString(16)}`.slice(-6)}`;
