d528db0ae3512ca2934948070799818cfd4671ad
MasonMS <brightcastle91@gmail.com>
2020-08-19 13:11:45 +0900

    API 추가 1. id array로 받아서 bulk update

--- front/src/modules/hangang/sagas.js
+++ front/src/modules/hangang/sagas.js
@@ -1,4 +1,4 @@
-import { getHangangTemp, getQuotes, getSubmitQuotes, getQuotesAdmin } from '../../lib/api/hangang';
+import { getHangangTemp, getQuotes, getSubmitQuotes, getQuotesAdmin, updateQuotesAccepted } from '../../lib/api/hangang';
 import { put, call, takeEvery } from 'redux-saga/effects';
 import {
   GET_HANGANG_TEMP_SUCCESS,
@@ -8,7 +8,9 @@ import {
   GET_QUOTES_REQUEST_ADMIN,
   GET_QUOTES_SUCCESS,
   GET_QUOTES_FAILURE,
-  GET_QUOTES_SUBMIT
+  GET_QUOTES_SUBMIT,
+
+  PUT_QUOTES_ACCEPTED
 } from './reducer';
 
 function* getHangangTempSaga(action) {
@@ -107,9 +109,34 @@ function* getSubmitQuotesSaga(action) {
   }
 }
 
+function* putQuotesAccepted(action) {
+  try {
+    console.log('[masonms] putQuotesAccepted: ', action);
+    const quotesAcceptedData = yield call(updateQuotesAccepted, action.payload);
+    console.log('[masonms] quotesAcceptedData: ', quotesAcceptedData);
+    yield put({
+      type: GET_QUOTES_SUCCESS,
+      payload: {
+        loading: false,
+        data: quotesAcceptedData.data,
+        error: null
+      }
+    });
+  } catch (e) {
+    yield put({
+      type: GET_QUOTES_FAILURE,
+      payload: {
+        loading: false,
+        data: [],
+        error: e
+      }
+    });
+  }
+}
 export function* hangangSaga() {
   yield takeEvery(GET_HANGANG_TEMP_REQUEST, getHangangTempSaga);
   yield takeEvery(GET_QUOTES_REQUEST, getQuotesSaga);
   yield takeEvery(GET_QUOTES_SUBMIT, getSubmitQuotesSaga);
   yield takeEvery(GET_QUOTES_REQUEST_ADMIN, getQuotesSagaAdmin);
+  yield takeEvery(PUT_QUOTES_ACCEPTED, putQuotesAccepted);
 }
