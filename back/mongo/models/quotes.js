da96704ac9a426999092a3dd1fac197a59ea7a47
MasonMS <brightcastle91@gmail.com>
2020-08-27 15:15:24 +0900

    스키마 구조변경 등 추가

--- back/mongo/models/quotes.js
+++ back/mongo/models/quotes.js
@@ -5,15 +5,15 @@ const Schema = mongoose.Schema;
 
 // Define Schemes
 const quotesSchema = new Schema({
-    name: { type: String, required: true },
-    word: { type: String, required: true },
-    thumbnail_user_image: { type: String, required: false, default: '' },
-    thumbnail_background_image: { type: String, required: false, default: '' },
-    accepted: { type: String, required: false, default: '1' },   // 0: 승인, 1: 대기, 2: 거절
-    insert_time: { type: String, default: moment().tz('Asia/Seoul').format('YYYY-MM-DD HH:mm:ss') },
-    updat_time: { type: String, default: moment().tz('Asia/Seoul').format('YYYY-MM-DD HH:mm:ss') },
-    status: { type: String, default: '0'},   // 소재상태 0: 사용중, 1: 삭제됨
-    card_exps_typ_cd: { type: String, require: false, default: '10'}    // 카드 노출 타입 10: 기본, 20: 세로카드블럭, 30: 썸네일카드블럭
+    NAME: { type: String, required: true },
+    WORD: { type: String, required: true },
+    THUMBNAIL_USER_IMAGE: { type: String, required: false, default: '' },
+    THUMBNAIL_BACKGROUND_IMAGE: { type: String, required: false, default: '' },
+    ACCEPTED: { type: String, required: false, default: '1' },   // 0: 승인, 1: 대기, 2: 거절
+    INSERT_TIME: { type: String, default: moment().tz('Asia/Seoul').format('YYYY-MM-DD HH:mm:ss') },
+    UPDATE_TIME: { type: String, default: moment().tz('Asia/Seoul').format('YYYY-MM-DD HH:mm:ss') },
+    STATUS: { type: String, default: '0'},   // 소재상태 0: 사용중, 1: 삭제됨
+    CARD_EXPS_TYP_CD: { type: String, require: false, default: '10'}    // 카드 노출 타입 10: 기본, 20: 세로카드블럭, 30: 썸네일카드블럭
 }, {
     collection: 'QUOTES'
 });
