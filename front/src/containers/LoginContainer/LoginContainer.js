b9c3e5230ca55318df492c7b4519f3cba5799a08
MasonMS <brightcastle91@gmail.com>
2020-09-09 18:34:13 +0900

    카카오톡 로그인 관련 로직 수정진행중

--- front/src/containers/LoginContainer/LoginContainer.js
+++ front/src/containers/LoginContainer/LoginContainer.js
@@ -2,20 +2,9 @@ import React, { useEffect, useState } from 'react';
 import KakaoLoginComponent from '../../component/KakaoLoginComponent';
 
 function LoginContainer() {
-    const loginSuccess = (data) => {
-        console.log('login success: ', data);
-    }
-
-    const loginFail = (data) => {
-        console.log('login fail: ', data);
-    }
-
     return(
         <div>
-            <KakaoLoginComponent
-                loginSuccess={loginSuccess}
-                loginFail={loginFail}
-            />
+            <KakaoLoginComponent/>
         </div>
     )
 }
