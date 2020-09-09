f08c6548e9a37889de7aa296bc3dae4ef107f87f
seoyeon kim <westkitedev@gmail.com>
2020-09-09 18:40:39 +0900

    feat : google login 준비중,  서버에서 검증 절차 구현해야함 ps. favicon 추가

---
+++ front/src/component/GoogleLoginComponent/GoogleLoginComponent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+
+const GoogleLoginComponent = (props) => {
+  const { loginSuccess, loginFail } = props;
+  const responseGoogle = (response) => {
+    console.log(response);
+  };
+  return (
+    <GoogleLogin
+      clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+      buttonText="Login"
+      onSuccess={loginSuccess}
+      onFailure={loginFail}
+      cookiePolicy={'single_host_origin'}
+    />
+  );
+};
+
+export default GoogleLoginComponent;
--- front/src/containers/LoginContainer/LoginContainer.js
+++ front/src/containers/LoginContainer/LoginContainer.js
@@ -1,23 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import KakaoLoginComponent from '../../component/KakaoLoginComponent';
-
+import GoogleLoginComponent from '../../component/GoogleLoginComponent';
 function LoginContainer() {
-    const loginSuccess = (data) => {
-        console.log('login success: ', data);
-    }
+  const loginSuccess = (data) => {
+    console.log('login success: ', data);
+  };
 
-    const loginFail = (data) => {
-        console.log('login fail: ', data);
-    }
+  const loginFail = (data) => {
+    console.log('login fail: ', data);
+  };
 
-    return(
-        <div>
-            <KakaoLoginComponent
-                loginSuccess={loginSuccess}
-                loginFail={loginFail}
-            />
-        </div>
-    )
+  return (
+    <div>
+      <KakaoLoginComponent loginSuccess={loginSuccess} loginFail={loginFail} />
+      <GoogleLoginComponent loginSuccess={loginSuccess} loginFail={loginFail} />
+    </div>
+  );
 }
 
 export default LoginContainer;
