95ddd0a71824ad5218b815166f22587861ed285a
westkite1201 <westkite1201@gmail.com>
2020-09-20 22:44:07 +0900

    fix : background, quotes 수정 background time 분기처리 오류 수정 quotesGridView top-bottom margin으로 style 변경처리

--- front/src/containers/QuotesGridViewComponent/QuotesGridViewComponent.js
+++ front/src/containers/QuotesGridViewComponent/QuotesGridViewComponent.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import { Grid } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import { GET_QUOTES_REQUEST } from '../../modules/hangang/reducer';
 import QuotesCard from '../../component/QuotesCard';
-
+import styled from 'styled-components';
 const QuotesCardList = ({ quotesList }) => {
   return quotesList.map((quotes, key) => {
     return <QuotesCard quotes={quotes} key={key} />;
@@ -24,17 +24,23 @@ const QuotesGridViewComponent = () => {
   }, [dispatch]);
 
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={1} md={1} lg={1}></Grid>
-      <Grid item xs={10} md={10} lg={10}>
-        <Grid container spacing={3}>
-          {quotesList && quotesList.length !== 0 && (
-            <QuotesCardList quotesList={quotesList} />
-          )}
+    <QuotesWrapper>
+      <Grid container spacing={3}>
+        <Grid item xs={1} md={1} lg={1}></Grid>
+        <Grid item xs={10} md={10} lg={10}>
+          <Grid container spacing={3}>
+            {quotesList && quotesList.length !== 0 && (
+              <QuotesCardList quotesList={quotesList} />
+            )}
+          </Grid>
         </Grid>
+        <Grid item xs={1} md={1} lg={1}></Grid>
       </Grid>
-      <Grid item xs={1} md={1} lg={1}></Grid>
-    </Grid>
+    </QuotesWrapper>
   );
 };
+const QuotesWrapper = styled.div`
+  margin-top: 1.5rem;
+  margin-bottom: 1.5rem;
+`;
 export default QuotesGridViewComponent;
