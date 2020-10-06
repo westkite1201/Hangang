3db2c6cc28a946e788851e2ae3406a1f5c5e3b6b
seoyeon kim <westkitedev@gmail.com>
2020-10-06 10:19:09 +0900

    Migration : 불 필요 디렉토리삭제 temp saga  변경 처리 완료

--- front/src/modules/admin/types.ts
+++
@@ -1,9 +0,0 @@
-import { ActionType } from 'typesafe-actions';
-import * as actions from './actions';
-import { HangangTemp } from '../../lib/api/hangang';
-import { AsyncState } from '../../lib/utils/reducerUtils';
-export type TodosAction = ActionType<typeof actions>;
-
-export type HangangState = {
-  userTodos: AsyncState<HangangTemp, Error>;
-};
