9cde4070649ce16cedd1db2b294cb2fad5fa10ad
seoyeon kim <westkitedev@gmail.com>
2020-10-16 15:25:07 +0900

    Feat : quotes manage update 완성

--- front/src/modules/quotes_ts/sagas.ts
+++ front/src/modules/quotes_ts/sagas.ts
@@ -1,10 +1,17 @@
-import { UPDATE_QUOTES_SUCCESS,UPDATE_QUOTES_REQUEST,updateQuotesAsync } from './reducer';
+import { 
+  UPDATE_QUOTES_SUCCESS,
+  UPDATE_QUOTES_REQUEST,
+  updateQuotesAsync,
+  GET_QUOTES_REQUEST,
+  GET_QUOTES_SUCCESS,
+  getQuotesAsync
+   } from './reducer';
 //import createAsyncSaga from '../../lib/utils/createAsyncSaga';
-import {updateQuote,QuoteResData} from '../../lib/api/quotes_saga';
+import {QuoteResData,updateQuote, getQuotes} from '../../lib/api/quotes_saga';
 import { put, call, takeEvery } from 'redux-saga/effects';
 
 //const updatqQuotesSaga = createAsyncSaga(updateQuotesAsync, updateQuote);
-function* updatqQuotesSaga(action: ReturnType<typeof updateQuotesAsync.request>) {
+function* updateQuotesSaga(action: ReturnType<typeof updateQuotesAsync.request>) {
   try {
     const quotesRes: QuoteResData = yield call(updateQuote, action.payload);
     console.log('quotesRes ', quotesRes);
@@ -20,9 +27,25 @@ function* updatqQuotesSaga(action: ReturnType<typeof updateQuotesAsync.request>)
   }
 }
 
+function* getQuotesSaga(action: ReturnType<typeof getQuotesAsync.request>) {
+  try {
+    const quotesRes: QuoteResData = yield call(getQuotes, action.payload);
+    console.log('quotesRes ', quotesRes);
+    yield put({
+      type: GET_QUOTES_SUCCESS,
+      payload: { quotes_array: quotesRes.quotes_array , totalCount : quotesRes.total_count}
+    });
+  } catch (e) {
+    yield put({
+      type: updateQuotesAsync.failure,
+      payload: { message: e.message }
+    });
+  }
+}
 
 export function* quotesSaga_() {
-  yield takeEvery(UPDATE_QUOTES_REQUEST, updatqQuotesSaga);
+  yield takeEvery(UPDATE_QUOTES_REQUEST, updateQuotesSaga);
+  yield takeEvery(GET_QUOTES_REQUEST, getQuotesSaga);
 }
 
 
