a9a34cf86413d46b23cd695ea6bde9611958abe5
MasonMS <brightcastle91@gmail.com>
2020-10-16 11:44:22 +0900

    feat: 카드 submit 시 현재 카드갯수를 기준으로 하여 CARD_ORDER를 지정

--- back/mongo/models/quotes.js
+++ back/mongo/models/quotes.js
@@ -21,7 +21,8 @@ const quotesSchema = new Schema(
       default: moment().tz('Asia/Seoul').format('YYYY-MM-DD HH:mm:ss')
     },
     STATUS: { type: String, default: '0' }, // 소재상태 0: 사용중, 1: 삭제됨
-    CARD_EXPS_TYP_CD: { type: String, require: false, default: '10' } // 카드 노출 타입 10: 기본, 20: 세로카드블럭, 30: 썸네일카드블럭
+    CARD_EXPS_TYP_CD: { type: String, require: false, default: '10' }, // 카드 노출 타입 10: 기본, 20: 세로카드블럭, 30: 썸네일카드블럭
+    CARD_ORDER: { type: Number, require: false }  // 카드 정렬 순서
   },
   {
     collection: 'QUOTES'
